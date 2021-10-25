0b32c59d92b618aa2937af4b9b2d9034f8abd5a4
falves1 <akulla.pro@gmail.com>
2021-10-25 11:38:46 +0200

    Frog

--- src/frog/Frog.java
+++ src/frog/Frog.java
@@ -3,13 +3,15 @@ package frog;
 import gameCommons.Game;
 import gameCommons.IFrog;
 import util.Case;
+import util.Direction;
 
-public class Frog {
+public class Frog implements IFrog {
 	
 	private Game game;
 	private boolean alive = true;
 	private int aliveTicks = 0;
 	private Case pos;
+	private Direction dir;
 
 	public void setAlive(boolean state){
 		alive = state;
@@ -29,5 +31,17 @@ public class Frog {
 
 	public Frog(Game game){
 		this.game = game;
+	}
+
+	public Case getPosition(){
+		return pos;
+	}
+
+	public Direction getDirection(){
+		return dir;
+	}
+
+	public void move(Direction key){
+		this.dir = key;
 	}
 }
--- src/gameCommons/Game.java
+++ src/gameCommons/Game.java
@@ -1,10 +1,11 @@
 package gameCommons;
 
-import java.awt.Color;
-import java.util.Random;
-
 import graphicalElements.Element;
 import graphicalElements.IFroggerGraphics;
+import util.Case;
+
+import java.awt.*;
+import java.util.Random;
 
 public class Game {
 
@@ -16,7 +17,7 @@ public class Game {
 	public final int minSpeedInTimerLoops;
 	public final double defaultDensity;
 
-	// Lien aux objets utilisés
+	// Lien aux objets utilisï¿½s
 	private IEnvironment environment;
 	private IFrog frog;
 	private IFroggerGraphics graphic;
@@ -30,7 +31,7 @@ public class Game {
 	 * @param height
 	 *            hauteur en cases
 	 * @param minSpeedInTimerLoop
-	 *            Vitesse minimale, en nombre de tour de timer avant déplacement
+	 *            Vitesse minimale, en nombre de tour de timer avant dï¿½placement
 	 * @param defaultDensity
 	 *            densite de voiture utilisee par defaut pour les routes
 	 */
@@ -44,7 +45,7 @@ public class Game {
 	}
 
 	/**
-	 * Lie l'objet frog à la partie
+	 * Lie l'objet frog ï¿½ la partie
 	 * 
 	 * @param frog
 	 */
@@ -70,21 +71,21 @@ public class Game {
 	}
 
 	/**
-	 * Teste si la partie est perdue et lance un écran de fin approprié si tel
+	 * Teste si la partie est perdue et lance un ï¿½cran de fin appropriï¿½ si tel
 	 * est le cas
 	 * 
 	 * @return true si le partie est perdue
 	 */
 	public boolean testLose() {
-		// TODO
+		Case pos = this.frog.getPosition();
 		return false;
 	}
 
 	/**
-	 * Teste si la partie est gagnee et lance un écran de fin approprié si tel
+	 * Teste si la partie est gagnee et lance un ï¿½cran de fin appropriï¿½ si tel
 	 * est le cas
 	 * 
-	 * @return true si la partie est gagnée
+	 * @return true si la partie est gagnï¿½e
 	 */
 	public boolean testWin() {
 		// TODO
